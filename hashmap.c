c34220e60db3df21c410928b3264bacd388daa1e
Your Name <you@example.com>
2026-04-15 19:30:13 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -137,6 +137,7 @@ Pair * searchMap(HashMap * map,  char * key)
             posicion++;
         }
 }
+}
 
 // 4. Implemente la función void eraseMap(HashMap * map, char * key). 
 // Está función elimina el dato correspondiente a la clave key. 
