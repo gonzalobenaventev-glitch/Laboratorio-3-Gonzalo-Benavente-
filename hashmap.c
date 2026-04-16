12e03af83126057f0c462db641958061f4afe10d
Your Name <you@example.com>
2026-04-16 13:51:57 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -186,7 +186,7 @@ Pair * firstMap(HashMap * map)
         {
             if (map->buckets[i]->key != NULL)
             {
-                map->buckets[map->current] = map->buckets[i];
+                map->current = i;
 
                 return map->buckets[map->current];
             }
